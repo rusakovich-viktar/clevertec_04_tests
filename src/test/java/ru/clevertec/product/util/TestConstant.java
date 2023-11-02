d5d0562a8b6b96caea310347006ba0cd75f8d71f
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-11-02 23:22:18 +0300

    feat: add mapperTests

---
+++ src/test/java/ru/clevertec/product/util/TestConstant.java
@@ -0,0 +1,12 @@
+package ru.clevertec.product.util;
+
+import java.util.UUID;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestConstant {
+
+    public static final String UUID_MY = "e8ff854a-e951-4c50-87c1-03a8657f6f28";
+
+    public static final UUID CORRECT_UUID = UUID.fromString("29064a8e-e53c-44eb-9cd4-ddd76958a878");
+}
